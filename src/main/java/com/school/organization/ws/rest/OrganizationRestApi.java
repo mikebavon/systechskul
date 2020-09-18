b2a2c7baa3ac91bbb1f024a1411c0eef0e37830c
mikebavon okello <bavomike@gmail.com>
2020-09-18 15:44:32 +0300

    -rest to save

--- src/main/java/com/school/organization/ws/rest/OrganizationRestApi.java
+++ src/main/java/com/school/organization/ws/rest/OrganizationRestApi.java
@@ -1,11 +1,10 @@
 package com.school.organization.ws.rest;
 
 import com.school.organization.bean.OrganizationBeanI;
+import com.school.organization.model.skul.Organization;
 
 import javax.ejb.EJB;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -16,10 +15,26 @@ public class OrganizationRestApi {
     private OrganizationBeanI organizationBean;
 
     @GET
-    @Path("/list")
+    @Path("/list/{name}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response list(){
+    public Response list(@PathParam(value = "name") String name){
+
+        System.out.println("Query param name: " + name);
         return Response.status(200).entity(organizationBean.list()).build();
+    }
+
+    @POST
+    @Path("/save")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String save(Organization organization){
+        try {
+            organizationBean.add(organization);
+        }catch (Exception ex){
+            return "{\"FAILURE\":\"" + ex.getMessage() + "\"}";
+        }
+
+        return "{\"SUCCESS\":\"OK\"}";
     }
 
 
